be87b6bc5110f086f035adb39bdbec83623f5898
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 19:13:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,50 @@ client.on('ready', () => {
 
 client.on('message', message => {
 if (message.content === "test") {
-  var ms = 600000;
+  var ms = 60000;
       var interval = setInterval (function () {
         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
       }, ms); 
     }
+});
+
+clien.on('message', message => {
+      if(message.content == '!قران 1'){
+        message.react('🔊')}  return;
+  const ytdl = require('ytdl-core');
+    const stram0ptions = { seek: 0, volume: 100};  
+    const broadcast = 
+clien.createVoiceBroadcast();
+
+
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+ })
+.catch(console.error);
+});
+
+clien.on('messge', message => {
+       if(message.content == '!قران 2'){
+           message.react('🔊')} return;
+    const ytdl = require('ytdl-core');
+      const stram0ptions = { seek: 0, volume:100};
+      const broadcast = 
+clien.createVoiceBroadcast();
+
+
+
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=E1vpbnkTmUE' ,{ filter : 'audioonly' });
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+       })
+.catch(console.error);
 });
 
 client.login(process.env.BOT_TOKEN); 
