3fcbb7b2abd675736f88d7e3b780da743646cd61
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 22:06:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -201,7 +201,8 @@ if (message.content === "test") {
     }, ms);1000
 
 });
-`client.on('message', message => {
+`
+client.on('message', message => {
   if (true) {
 if (message.content === "$invite") {
       message.author.send('  رابط بوتك  | https://goo.gl/xdM1k9     ').catch(e => console.log(e.stack));
@@ -212,7 +213,7 @@ if (message.content === "$invite") {
  
  
 client.on('message', message => {
-     if (message.content === "-inv") {
+     if (message.content === "!invite") {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
