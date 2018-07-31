ba7a192a77178398865b05934e8b39080e6328e0
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 22:03:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -201,5 +201,27 @@ if (message.content === "test") {
     }, ms);1000
 
 });
+`client.on('message', message => {
+  if (true) {
+if (message.content === "$invite") {
+      message.author.send('  رابط بوتك  | https://goo.gl/xdM1k9     ').catch(e => console.log(e.stack));
+ 
+    }
+   }
+  });
+ 
+ 
+client.on('message', message => {
+     if (message.content === "-inv") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
 
 client.login(process.env.BOT_TOKEN); 
