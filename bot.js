1f2aab328e3b06747f6892875268de8980c8cf57
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 19:02:42 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "!";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+if (message.content === "test") {
+  var ms = 600000;
+      var interval = setInterval (function () {
+        message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
+      }, ms); 
+    }
+});
