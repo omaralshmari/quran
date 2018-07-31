17e6dee6c5fba79ac79d08d9a1f992e3750be502
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 19:19:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,43 +15,5 @@ if (message.content === "test") {
     }
 });
 
-clien.on('message', message => {
-      if(message.content == '!قران 1'){
-        message.react('🔊')}  return;
-  const ytdl = require('ytdl-core');
-    const stram0ptions = { seek: 0, volume: 100};  
-    const broadcast = 
-clien.createVoiceBroadcast();
-
-
-
-message.gulid.member(message.author).voiceChannel.join()
-       .then(connection => {
-           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
-           broadcast.playstream(stream);
-           const dispatcher = connection.playBroadcast(broadcast);
- })
-.catch(console.error);
-});
-
-clien.on('messge', message => {
-       if(message.content == '!قران 2'){
-           message.react('🔊')} return;
-    const ytdl = require('ytdl-core');
-      const stram0ptions = { seek: 0, volume:100};
-      const broadcast = 
-clien.createVoiceBroadcast();
-
-
-
-
-message.gulid.member(message.author).voiceChannel.join()
-       .then(connection => {
-           const stream = ytdl('https://www.youtube.com/watch?v=E1vpbnkTmUE' ,{ filter : 'audioonly' });
-           broadcast.playstream(stream);
-           const dispatcher = connection.playBroadcast(broadcast);
-       })
-.catch(console.error);
-});
 
 client.login(process.env.BOT_TOKEN); 
